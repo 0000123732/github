a191e4afd55216f8957a231521184a090d08e442
0000123732 <25s10523@guro.sen.hs.kr>
2026-07-20 13:49:59 +0900

    Fix typo in session state variable name

--- ddt54.py
+++ ddt54.py
@@ -4,5 +4,5 @@ st.title("카운터 앱")
 if 'count' not in st.session_state:
     st.session state.counte = 0
 if st.button("증가"):
-   st.session_state.count += 1
+    st.session_state.count += 1
 st.markdown (f"## 현재 숫지: "{st.session_state.count}'")
