68f8ada22a512bd80733c6d61efa718768e5d21c
0000123732 <25s10523@guro.sen.hs.kr>
2026-07-20 13:51:49 +0900

    Fix typo in session state and markdown output

--- ddt54.py
+++ ddt54.py
@@ -5,4 +5,4 @@ if 'count' not in st.session_state:
     st.session_state.counte = 0
 if st.button("증가"):
     st.session_state.count += 1
-st.markdown (f"## 현재 숫지: "{st.session_state.count}'")
+st.markdown(f"## 현재 숫지:  '{st.session_state.count}'")
