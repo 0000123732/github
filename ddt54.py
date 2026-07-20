44d53d0e11279c0d2b1e56e9b2958722f4009083
0000123732 <25s10523@guro.sen.hs.kr>
2026-07-20 13:49:24 +0900

    Update ddt54.py

--- ddt54.py
+++ ddt54.py
@@ -1,7 +1,8 @@
 import streamlit as st
 
 st.title("카운터 앱")
-count = 0
+if 'count' not in st.session_state:
+    st.session state.counte = 0
 if st.button("증가"):
-    count = count+1
-st.markdown(f"## 현재 숫자: '{count}'")
+   st.session_state.count += 1
+st.markdown (f"## 현재 숫지: "{st.session_state.count}'")
