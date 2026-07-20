b26f4d8a4ea06eb93d8db9c36dd3974c6d4c1f6d
0000123732 <25s10523@guro.sen.hs.kr>
2026-07-20 13:50:54 +0900

    Fix typo in session state initialization

--- ddt54.py
+++ ddt54.py
@@ -2,7 +2,7 @@ import streamlit as st
 
 st.title("카운터 앱")
 if 'count' not in st.session_state:
-    st.session state.counte = 0
+    st.session_state.counte = 0
 if st.button("증가"):
     st.session_state.count += 1
 st.markdown (f"## 현재 숫지: "{st.session_state.count}'")
