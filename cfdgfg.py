405d0f6f73d16b42f06921daa14dd36b1d969b6f
0000123732 <25s10523@guro.sen.hs.kr>
2026-07-21 09:40:46 +0900

    Update cfdgfg.py

--- cfdgfg.py
+++ cfdgfg.py
@@ -33,15 +33,12 @@ st.markdown("---")
 
      for i in range(len(st.session_state.todo_list)):
      col_task, col_btn, col_status = st.columns([4, 1, 1])
-      
      with col_task:
-        st.write(f"{i+1}. {st.session_state.todo_list[i][0]}")
-        
+        st.write(f"{i+1}. {st.session_state.todo_list[i][0]}")  
      with col_btn:
         if st.button("완료", key=f"btn_{i}"):
             st.session_state.todo_list[i][1] = True
-            st.rerun()
-            
+            st.rerun()            
      with col_status:
             if st.session_state.todo_list[i][1]:
             st.write("✅ **달성!**")
