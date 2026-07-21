b2763d1bd3dec5b69148c35b33c509fe2701b26e
0000123732 <25s10523@guro.sen.hs.kr>
2026-07-21 09:39:02 +0900

    Update cfdgfg.py

--- cfdgfg.py
+++ cfdgfg.py
@@ -24,11 +24,10 @@ def Page_motto():
 def page_todo():
     st.header("✅ 2. 오늘의 할 일")
     st.write(f"현재 다짐: **{st.session_state.user_motto}**")
-new_todo = st.text_input("추가할 할 일을 입력하세요", key="todo_input")
-st.button("추가하기", on_click=add_todo)
-
-  if new_todo == "":
-    st.warning("할 일을 입력하고 버튼을 눌러주세요!")
+    new_todo = st.text_input("추가할 할 일을 입력하세요", key="todo_input")
+    st.button("추가하기", on_click=add_todo)
+    if new_todo == "":
+       st.warning("할 일을 입력하고 버튼을 눌러주세요!")
 
 st.markdown("---")
 
