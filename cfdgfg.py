155b7eb5f4793de9478cae35ced3ac682f601756
0000123732 <25s10523@guro.sen.hs.kr>
2026-07-21 09:24:01 +0900

    Update cfdgfg.py

--- cfdgfg.py
+++ cfdgfg.py
@@ -12,6 +12,7 @@ def add_todo():
         st.toast("할 일이 추가되었습니다!")
         st.session_state.todo_input = ""
 
+def page_motto():
 st.title("🌱 갓생 살기 플래너")
 st.header("📣 1. 오늘의 다짐")
 motto = st.text_input("나의 한 줄 좌우명을 적어주세요")
@@ -19,7 +20,7 @@ if st.button("다짐 저장"):
     st.session_state.user_motto = motto
     st.success("좌우명이 등록되었습니다!")
 st.markdown("---")
-
+def page_todo():
 st.header("✅ 2. 오늘의 할 일")
 st.write(f"현재 다짐: **{st.session_state.user_motto}**")
 new_todo = st.text_input("추가할 할 일을 입력하세요", key="todo_input")
@@ -40,7 +41,7 @@ for i in range(len(st.session_state.todo_list)):
         if st.session_state.todo_list[i][1]:
             st.write("✅ **달성!**")
 st.markdown("---")
-
+def page_report():
 st.header("📈 3. 나의 갓생 지수")
 if not st.session_state.todo_list:
     st.write("아직 등록된 할 일이 없습니다.")
@@ -56,3 +57,11 @@ else:
     if st.button("기록 전체 초기화"):
         st.session_state.todo_list = []
         st.rerun()
+
+pg = st.navigation([
+    st.Page(page_motto, title="오늘의 다짐", icon="📣"),
+    st.Page(page_todo, title="오늘의 할 일", icon="✅"),
+    st.Page(page_report, title="나의 갓생 지수", icon="📈")])
+
+    st.title("🌱 갓생 살기 플래너")
+    pg.run()
