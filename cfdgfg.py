947bc743479b32f837abddcaf7bb3125a77273cd
0000123732 <25s10523@guro.sen.hs.kr>
2026-07-21 09:59:32 +0900

    Update cfdgfg.py

--- cfdgfg.py
+++ cfdgfg.py
@@ -30,36 +30,36 @@ def page_todo():
        st.warning("할 일을 입력하고 버튼을 눌러주세요!")
        st.markdown("---")
     for i in range(len(st.session_state.todo_list)):
-    col_task, col_btn, col_status = st.columns([4, 1, 1])
-    with col_task:
-        st.write(f"{i+1}. {st.session_state.todo_list[i][0]}")
-    with col_btn:
-        if st.button("완료", key=f"btn_{i}"):
-            st.session_state.todo_list[i][1] = True
-            st.rerun()
-    with col_status:
-         if st.session_state.todo_list[i][1]:
-            st.write("✅ **달성!**")
-            st.markdown("---")
+        col_task, col_btn, col_status = st.columns([4, 1, 1])
+        with col_task:
+            st.write(f"{i+1}. {st.session_state.todo_list[i][0]}")
+        with col_btn:
+            if st.button("완료", key=f"btn_{i}"):
+                st.session_state.todo_list[i][1] = True
+                st.rerun()
+        with col_status:
+             if st.session_state.todo_list[i][1]:
+                st.write("✅ **달성!**")
+    st.markdown("---")
 
 def page_report():
     st.header("📈 3. 나의 갓생 지수")
     if not st.session_state.todo_list:
            st.write("아직 등록된 할 일이 없습니다.")
-           else:
-    total = len(st.session_state.todo_list)
-    count = 0
-    for item in st.session_state.todo_list:
-        if item[1] == True:
-            count += 1
-    progress = (count / total) * 100
-    st.metric("오늘의 달성률", f"{progress:.1f}%")
-    st.progress(progress / 100)
-    if st.button("기록 전체 초기화"):
-        st.session_state.todo_list = []
-        st.rerun()
+    else:
+        total = len(st.session_state.todo_list)
+        count = 0
+        for item in st.session_state.todo_list:
+            if item[1] == True:
+                count += 1
+        progress = (count / total) * 100
+        st.metric("오늘의 달성률", f"{progress:.1f}%")
+        st.progress(progress / 100)
+        if st.button("기록 전체 초기화"):
+            st.session_state.todo_list = []
+            st.rerun()
         
-        pg = st.navigation([
+pg = st.navigation([
     st.Page(page_motto, title="오늘의 다짐", icon="📣"),
     st.Page(page_todo, title="오늘의 할 일", icon="✅"),
     st.Page(page_report, title="나의 갓생 지수", icon="📈")])
